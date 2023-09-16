7fab937e4ea053c193caa066931d22b57a37ccb5
devdom90 <devdom@protonmail.com>
2023-09-16 10:37:44 +0200

    update threading

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import threading
 import socket
 
-
+username = input("Wähle einen Benutzername...")
 
 base = "127.0.0.1"
 
@@ -13,8 +13,27 @@ def get_content():
         try:
             message = client.recv(1024).decode('ascii')
             if message == "USER":
+                client.send(username.encode('ascii'))
                 pass
             else:
                 print(message)
         except:
             print("Etwas ist schief gelaufen")
+            client.close()
+            break
+
+
+
+def create_content():
+    while True:
+        message = f'{username} sagt: {input("")}'
+        client.send(message.encode('ascii'))
+
+
+
+get_thread = threading.Thread(target=get_content)
+get_thread.start()
+post_thread = threading.Thread(target=create_content)
+post_thread.start()
+
+    
